3d6a2fc6bfe196c9786946a71305f1f3d5042575
anita <arav12378@gmail.com>
2021-02-14 18:06:52 -0600

    updated test1

--- vars/terra.groovy
+++ vars/terra.groovy
@@ -1,20 +1,19 @@
-def call(){
+def call () {
 
 pipeline {
     agent any
 
     stages {
-        
-            
+                    
         stage('terraform init') {
             steps {
-                sh 'cd test; terraform init'
+                sh 'cd test1; terraform init'
             }
         }
         
         stage('terraform apply') {
             steps {
-                sh 'cd test; terraform apply -auto-approve'
+                sh 'cd test1; terraform apply -auto-approve'
             }
         }
     }
